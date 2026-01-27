b51a11a22aee246a6ae9f75fee969efd5df7a644
barisgzlx <baropqsawt@gmail.com>
2026-01-27 20:33:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
-// Statik dosyaları (html, js, css) sunmak için
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-let players = {};
-
-io.on('connection', (socket) => {
-    console.log('Yeni oyuncu:', socket.id);
-    players[socket.id] = { x: 500, y: 500, color: 'red' };
-    io.emit('updatePlayers', players);
-
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('updatePlayers', players);
-    });
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Sunucu ${PORT} portunda hazir!`);
+app.listen(PORT, () => {
+  console.log('Sunucu ayakta!');
 });
