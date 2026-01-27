f94a6fe3b3fd1a80c0aa2c522a26d84733acf9d3
barisgzlx <baropqsawt@gmail.com>
2026-01-27 20:36:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Dosyaları ana dizinden oku
 app.use(express.static(__dirname));
 
+// Ana sayfayı açınca index.html'i göster
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Sunucu ayakta!');
+  console.log('Sunucu calisiyor!');
 });
