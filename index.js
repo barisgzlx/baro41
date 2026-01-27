24402d14d02ae267e5e7c0f24972c74afa1326fd
barisgzlx <baropqsawt@gmail.com>
2026-01-27 21:20:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Dosyaları dışarı aç
 app.use(express.static(__dirname));
 
-// Tüm oyuncuları burada tutacağız
-let players = {};
+// Odalara göre oyuncuları ayırıyoruz
+let rooms = {
+    ffa1: {},
+    ffa2: {}
+};
 
 io.on('connection', (socket) => {
-    console.log('Bir oyuncu bağlandı:', socket.id);
-    
-    // Yeni bağlanan oyuncuya rastgele bir başlangıç yeri ver
-    players[socket.id] = { 
-        x: Math.random() * 800, 
-        y: Math.random() * 600, 
-        radius: 20, 
-        color: 'dodgerblue' 
-    };
+    socket.on('join', (roomName) => {
+        socket.join(roomName);
+        socket.currentRoom = roomName;
 
-    // Herkese güncel oyuncu listesini duyur
-    io.emit('updatePlayers', players);
+        rooms[roomName][socket.id] = { 
+            x: Math.random() * 800, 
+            y: Math.random() * 600, 
+            radius: 20, 
+            color: roomName === 'ffa1' ? 'dodgerblue' : 'limegreen' 
+        };
 
-    // Bir oyuncu faresini oynattığında gelen veriyi al
+        // Sadece o odadaki oyunculara listeyi gönder
+        io.to(roomName).emit('updatePlayers', rooms[roomName]);
+    });
+
     socket.on('move', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
-            players[socket.id].radius = data.radius; // Büyüme bilgisini de alalım
-            
-            // Yeni pozisyonu diğer tüm oyunculara gönder
-            io.emit('updatePlayers', players);
+        const room = socket.currentRoom;
+        if (room && rooms[room][socket.id]) {
+            rooms[room][socket.id].x = data.x;
+            rooms[room][socket.id].y = data.y;
+            rooms[room][socket.id].radius = data.radius;
+            io.to(room).emit('updatePlayers', rooms[room]);
         }
     });
 
-    // Oyuncu sayfayı kapattığında listeden sil
     socket.on('disconnect', () => {
-        console.log('Oyuncu ayrıldı:', socket.id);
-        delete players[socket.id];
-        io.emit('updatePlayers', players);
+        const room = socket.currentRoom;
+        if (room && rooms[room][socket.id]) {
+            delete rooms[room][socket.id];
+            io.to(room).emit('updatePlayers', rooms[room]);
+        }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log('Sunucu multiplayer modunda hazir!');
-});
+server.listen(PORT, () => console.log('Odali sistem hazir!'));
