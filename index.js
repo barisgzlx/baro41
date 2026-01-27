eb42becceacb437e0b8dd6f66c421fb9daddacf2
barisgzlx <baropqsawt@gmail.com>
2026-01-27 20:02:51 +0300

    index.js

--- index.js
+++ index.js
@@ -1,53 +1,17 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
-});
 const path = require('path');
 
+// Sunucunun HTML, CSS ve JS dosyalarını (script.js gibi) bulmasını sağlar
 app.use(express.static(__dirname));
 
-let players = {};
-let foods = [];
-
-// Başlangıç yemleri
-for(let i=0; i<150; i++) {
-    foods.push({ 
-        id: i, 
-        x: Math.random() * 3000, 
-        y: Math.random() * 3000, 
-        color: `hsl(${Math.random() * 360}, 70%, 50%)` 
-    });
-}
-
-io.on('connection', (socket) => {
-    players[socket.id] = {
-        id: socket.id,
-        x: 1500,
-        y: 1500,
-        radius: 30,
-        color: `hsl(${Math.random() * 360}, 80%, 60%)`,
-        name: "Oyuncu_" + socket.id.substr(0, 3)
-    };
-
-    socket.emit('init', { players, foods });
-    socket.broadcast.emit('newPlayer', players[socket.id]);
-
-    socket.on('move', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
-            socket.broadcast.emit('playerMoved', players[socket.id]);
-        }
-    });
-
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('removePlayer', socket.id);
-    });
+// Ana sayfaya (/) girildiğinde index.html dosyasını tarayıcıya gönderir
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-http.listen(3000, () => {
-    console.log('OYUN SUNUCUSU ÇALIŞIYOR: http://localhost:3000');
+// Render platformunun verdiği dinamik portu veya yerel test için 3000 portunu kullanır
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Oyun ${PORT} portunda yayında!`);
 });
